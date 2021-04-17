becb855bca89a9ce1824bcceb7ec505eb8a43289
Krisna <Krisna@LAPTOP-KLKE3D66>
2021-04-17 19:19:13 +0530

    DB Setup or initial code has completed and configure the application properties

---
+++ onlinebookstore/src/main/java/com/prashant/onlinebookstore/OnlinebookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.prashant.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookstoreApplication {
+
+	public static void main(String[] args) {
+		System.out.println("This is testing print line form the value oriented programming from the DB");
+		SpringApplication.run(OnlinebookstoreApplication.class, args);
+	}
+
+}
